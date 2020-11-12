740c13c0bff0804a4633b0dd2fedb3deeb10c15c
Jonathan Daughtry <daughtryje@gmail.com>
2020-11-12 17:43:12 -0600

    Saved project

--- SQLFiles Tier 1/SQLTasks_Tier_1.sql
+++ SQLFiles Tier 1/SQLTasks_Tier_1.sql
@@ -34,35 +34,64 @@ exploring the data, and getting acquainted with the 3 tables. */
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Write a SQL query to produce a list of the names of the facilities that do. */
 
+SELECT name
+FROM `Facilities`
+WHERE membercost =0;
 
 /* Q2: How many facilities do not charge a fee to members? */
 
+4
 
 /* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
 where the fee is less than 20% of the facility's monthly maintenance cost.
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
 
+SELECT facid, name, membercost, monthlymaintenance
+FROM Facilities
+WHERE membercost > 0.0
+AND membercost < 0.2 * monthlymaintenance;
 
 /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
 
+SELECT *
+FROM Facilities
+WHERE facid
+IN ( 1, 5 );
 
 /* Q5: Produce a list of facilities, with each labelled as
 'cheap' or 'expensive', depending on if their monthly maintenance cost is
 more than $100. Return the name and monthly maintenance of the facilities
 in question. */
 
+SELECT name,monthlymaintenance,
+	CASE WHEN monthlymaintenance > 100 THEN 'expensive'
+	ELSE 'cheap' END AS cost
+FROM Facilities;
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
 
+SELECT firstname, surname
+FROM Members
+WHERE joindate
+	IN (SELECT MAX( joindate )
+	FROM Members);
 
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
 
+SELECT DISTINCT f.name, CONCAT(m.firstname, ' ', m.surname) AS member_name
+FROM Bookings AS b
+INNER JOIN Members AS m 
+	ON b.memid = m.memid
+INNER JOIN Facilities AS f
+	ON b.facid = f.facid
+WHERE f.name LIKE 'Tennis Court%'
+ORDER BY member_name;
 
 /* Q8: Produce a list of bookings on the day of 2012-09-14 which
 will cost the member (or guest) more than $30. Remember that guests have
@@ -71,9 +100,37 @@ the guest user's ID is always 0. Include in your output the name of the
 facility, the name of the member formatted as a single column, and the cost.
 Order by descending cost, and do not use any subqueries. */
 
+SELECT f.name, CONCAT(Members.firstname, ' ', Members.surname), 
+CASE WHEN Members.memid !=0
+    THEN b.slots * f.membercost
+    ELSE b.slots * f.guestcost
+    END AS booking_cost
+FROM Bookings as b
+INNER JOIN Members ON b.memid = Members.memid
+INNER JOIN Facilities as f ON b.facid = f.facid
+WHERE b.starttime LIKE '2012-09-14%'
+  AND(
+    (b.memid != 0 AND b.slots * f.membercost > 30)
+    OR (b.memid = 0 AND b.slots * f.guestcost > 30))
+ORDER BY booking_cost DESC
 
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
 
+SELECT *
+FROM (
+  SELECT Facilities.name,
+  CONCAT(Members.firstname, ' ', Members.surname) AS member_name,
+  CASE WHEN Members.memid !=0
+    THEN Bookings.slots * Facilities.membercost
+    ELSE Bookings.slots * Facilities.guestcost
+    END AS booking_cost
+  FROM Bookings
+  INNER JOIN Members ON Bookings.memid = Members.memid
+  INNER JOIN Facilities ON Bookings.facid = Facilities.facid
+  WHERE Bookings.starttime LIKE '2012-09-14%'
+  ) AS sub
+WHERE sub.booking_cost > 30
+ORDER BY sub.booking_cost DESC
 
 /* PART 2: SQLite
 /* We now want you to jump over to a local instance of the database on your machine. 
@@ -95,11 +152,59 @@ QUESTIONS:
 The output of facility name and total revenue, sorted by revenue. Remember
 that there's a different cost for guests and members! */
 
+SELECT facility, total_revenue
+FROM (
+  SELECT Facilities.name AS facility,
+  SUM(
+    CASE WHEN Bookings.memid !=0
+    THEN Bookings.slots * Facilities.membercost
+    ELSE Bookings.slots * Facilities.guestcost END) AS total_revenue
+  FROM Facilities
+  INNER JOIN Bookings ON Bookings.facid = Facilities.facid
+  GROUP BY Facilities.name
+) AS facility_total_revenue
+WHERE total_revenue < 1000
+ORDER BY total_revenue
+
 /* Q11: Produce a report of members and who recommended them in alphabetic surname,firstname order */
 
+SELECT m1.surname, m1.firstname, m2.surname, m2.firstname
+FROM Members AS m1
+INNER JOIN Members AS m2
+ON m1.memid = m2.recommendedby
+WHERE m1.memid != 0
+ORDER BY m2.surname, m2.firstname
 
 /* Q12: Find the facilities with their usage by member, but not guests */
 
+SELECT f.name, CONCAT (m.firstname, ' ', m.surname), COUNT(f.name)
+FROM Bookings AS b
+INNER JOIN Facilities AS f
+on b.facid = f.facid
+INNER JOIN Members AS m
+on b.memid = m.memid
+WHERE m.memid != 0
+GROUP BY f.name, CONCAT (m.firstname, ' ', m.surname)
 
 /* Q13: Find the facilities usage by month, but not guests */
 
+SELECT f.name, CONCAT (m.firstname, ' ', m.surname), COUNT(f.name), 
+sum(case when month(starttime) = 1 then 1 else 0 end) as Jan,
+sum(case when month(starttime) = 2 then 1 else 0 end) as Feb,
+sum(case when month(starttime) = 3 then 1 else 0 end) as Mar,
+sum(case when month(starttime) = 4 then 1 else 0 end) as Apr,
+sum(case when month(starttime) = 5 then 1 else 0 end) as May,
+sum(case when month(starttime) = 6 then 1 else 0 end) as Jun,
+sum(case when month(starttime) = 7 then 1 else 0 end) as Jul,
+sum(case when month(starttime) = 8 then 1 else 0 end) as Aug,
+sum(case when month(starttime) = 9 then 1 else 0 end) as Sep,
+sum(case when month(starttime) = 10 then 1 else 0 end) as Oct,
+sum(case when month(starttime) = 11 then 1 else 0 end) as Nov,
+sum(case when month(starttime) = 12 then 1 else 0 end) as Dece
+FROM Bookings AS b
+INNER JOIN Facilities AS f
+on b.facid = f.facid
+INNER JOIN Members AS m
+on b.memid = m.memid
+WHERE m.memid != 0 AND year(starttime) = 2012
+GROUP BY f.name, CONCAT (m.firstname, ' ', m.surname);
